d4728f4814b97a5e70ae5572f28503bc958367c6
adrianpaliz <adrianpaliz@gmail.com>
2022-04-14 15:32:25 -0500

    Models file with logic of the application

--- record/models.py
+++ record/models.py
@@ -1,4 +1,6 @@
-import sqlite3
+import sqlite3, requests
+from config import API_KEY, URL_SPECIFIC_RATE
+from record.errors import APIError
 
 
 class ProcessData:
@@ -40,17 +42,32 @@ class ProcessData:
     def recover_data(self):
         return self.make_a_query(
             """
-                                    SELECT day, hour, currency_from, amount_from, currency_to, amount_to, id
-                                    FROM movements
-                                    ORDER BY day
-                                """
+            SELECT day, hour, currency_from, amount_from, currency_to, amount_to, id
+            FROM movements
+            ORDER BY day
+            """
         )
 
     def update_data(self, params):
         self.make_a_query(
             """
-                            UPDATE movements set day = ?, hour = ?, currency_from = ?, amount_from = ?, currency_to = ?, amount_to = ? 
-                            WHERE id = ?
-                        """,
+            UPDATE movements set day = ?, hour = ?, currency_from = ?, amount_from = ?, currency_to = ?, amount_to = ? 
+            WHERE id = ?
+            """,
             (params),
         )
+
+
+class APIRequest:
+    def __init__(self, currency_from="", currency_to=""):
+        self.currency_from = currency_from
+        self.currency_to = currency_to
+        self.rate = 0.0
+
+    def get_rate(self):
+        self.rate_request = requests.get(
+            URL_SPECIFIC_RATE.format(self.currency_from, self.currency_to, API_KEY)
+        )
+        # if self.rate_request.status_code != 200:
+        #    raise APIError(self.rate_request.json()["error"])
+        self.rate = round(self.rate_request.json()["rate"], 2)
