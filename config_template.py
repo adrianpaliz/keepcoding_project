db79f55e76299c04d9279ea3a17122527486d2bc
adrianpaliz <adrianpaliz@gmail.com>
2022-04-22 15:52:19 -0500

    Two constants are added

--- config_template.py
+++ config_template.py
@@ -1,2 +1,5 @@
-DATABASE_PATH="your database path"
-SECRET_KEY="your key"
+DATABASE_PATH = "your database path"
+SECRET_KEY = "your key"
+API_KEY = "your api key"
+URL_SPECIFIC_RATE = "https://rest.coinapi.io/v1/exchangerate/{}/{}?time={}&apikey={}"
+CURRENCIES = ['EUR', 'ETH', 'BTC', 'LUNA', 'SOL']
