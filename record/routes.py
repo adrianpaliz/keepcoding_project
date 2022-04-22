0d6ede9847ad71cb311b22ecf8d357399653a18b
adrianpaliz <adrianpaliz@gmail.com>
2022-04-22 15:47:36 -0500

    Logic added for: identify what submit button was clicked, for create the string for the API call, also for disable fields after submit was clicked, and for write in the database

--- record/routes.py
+++ record/routes.py
@@ -1,9 +1,9 @@
-import sqlite3
-from flask import render_template, flash, request
+from flask import redirect, render_template, request, url_for
 from record import app
 from record.models import ProcessData
 from record.forms import PurchaseForm
 from record.models import APIRequest
+import sqlite3
 
 database_path = app.config["DATABASE_PATH"]
 data_manager = ProcessData(database_path)
@@ -12,7 +12,7 @@ data_manager = ProcessData(database_path)
 @app.route("/")
 def home():
     data = data_manager.recover_data()
-    return render_template("transactions.html", movements=data)
+    return render_template("transactions.html", movements=data, navbar="Home")
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -20,28 +20,54 @@ def buy():
     instantiated_form = PurchaseForm()
 
     if request.method == "GET":
-        return render_template("buy.html", jinja_form=instantiated_form)
+        return render_template("buy.html", jinja_form=instantiated_form, navbar="Buy")
 
-    elif request.method == 'POST':
-        currency_from = request.form['currency_from']
-        currency_to = request.form['currency_to']
-        day = request.form['day']
-        hour = request.form['hour']        
-        
+    elif request.method == "POST" and instantiated_form.calculate.data == True:
+        currency_from = request.form["currency_from"]
+        currency_to = request.form["currency_to"]
+        day = request.form["day"]
+
+        hour = request.form["hour"]
+
         str_rate_time = day + "T" + hour
-        rate_time = str_rate_time     
 
-        form_api_request = APIRequest(currency_from, currency_to, rate_time)        
-        rate = form_api_request.get_rate()   
-                 
+        form_api_request = APIRequest(currency_from, currency_to, str_rate_time)
+        rate = form_api_request.get_rate()
         instantiated_form = PurchaseForm()
-        instantiated_form.unit_price = rate         
+        instantiated_form.unit_price.data = rate
 
-        return render_template("buy.html", jinja_form=instantiated_form)
+        amount_to_invest = instantiated_form.amount_from.data
+        instantiated_form.amount_to.data = rate * amount_to_invest
+
+        instantiated_form.currency_from.render_kw = {"disabled": "disabled"}
+        instantiated_form.currency_to.render_kw = {"disabled": "disabled"}
+        instantiated_form.amount_from.render_kw = {"disabled": "disabled"}
+
+        return render_template("buy.html", jinja_form=instantiated_form, navbar="Buy")
+
+    elif request.method == "POST" and instantiated_form.submit.data == True:
+        params = (
+            str(instantiated_form.day.strftime("%d/%m/%Y")),
+            str(instantiated_form.hour),
+            str(instantiated_form.currency_from.data),
+            str(instantiated_form.currency_to.data),
+            str(instantiated_form.amount_from.data),
+            str(instantiated_form.amount_to.data),
+            str(instantiated_form.unit_price.data),
+        )
+        data_manager.update_data(params)
+        return redirect(url_for("home"))
+
     else:
         raise Exception("Request method unknown")
 
 
 @app.route("/status")
 def status():
-    return render_template("status.html")
+
+    con = sqlite3.connect()
+    cur = con.cursor()
+
+    con.close()
+
+    return render_template("status.html", navbar="Status")
