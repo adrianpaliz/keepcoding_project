8f8b3dc28c2190a772b1a83bcc1008e97fc63f5a
adrianpaliz <adrianpaliz@gmail.com>
2022-05-03 17:19:24 -0500

    Default cryptocurrencies codes added

--- config_template.py
+++ config_template.py
@@ -2,9 +2,5 @@ DATABASE_PATH = "your database path"
 SECRET_KEY = "your key"
 API_KEY = "your api key"
 URL_SPECIFIC_RATE = "https://rest.coinapi.io/v1/exchangerate/{}/{}?time={}&apikey={}"
-CRYPTOCURRENCIES = [
-    "cryptocurrency_code1",
-    "cryptocurrency_code2",
-    "cryptocurrency_code3",
-]
+CRYPTOCURRENCIES = ["ETH", "BTC", "LUNA", "SOL"]
 CURRENCIES = ["EUR"] + CRYPTOCURRENCIES
